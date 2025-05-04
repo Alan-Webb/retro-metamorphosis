fc9787e87588ca1a8a1b6dba9a7bb1ba885b3270
Alan Webb <webbalan91939@gmail.com>
2025-05-04 18:53:05 +0100

    Initial commit

--- src/constants/index.jsx
+++
@@ -1,8 +0,0 @@
-export const LINKS = [
-	{name: "About", id: "about"},
-	{name: "Courses", id: "courses"},
-	{name: "Spells", id: "spells"},
-	{name: "Pricing", id: "pricing"},
-	{name: "Testimonials", id: "testimonials"},
-	{name: "Contact", id: "contact"},
-];
